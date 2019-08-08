387631fdb1771a8406313ececee0e90e8be3f8ea
Jan Roman Cisowski <janromancisowski@cisowscy.com>
2019-08-08 12:49:24 +0200

    Create server.module.ts

---
+++ src/server/server.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { AppServerController } from './server.controller';
+import { AppServerService } from './server.service';
+
+@Module({
+  imports: [],
+  controllers: [AppServerController],
+  providers: [AppServerService],
+})
+export class AppServerModule {}
